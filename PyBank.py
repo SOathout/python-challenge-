f27d40788d3022dbf061202fde78a724af7ebd80
SOathout <66280201+SOathout@users.noreply.github.com>
2020-07-24 18:13:48 -0700

    Add files via upload

--- PyBank.py
+++ PyBank.py
@@ -12,14 +12,31 @@ with open(csvpath) as csvfile:
 
     total_months = 0
     net_total = 0
-    avg_change = 0
-    great_incease = 0
-    great_decrease = 0
+    avgerage_change = 0
+    greatest_incease = 0
+    greatest_decrease = 0
     date = []
     profits =[]
 
-def print_percentages(first_row):
+ for row in csvreader:
+    date.append(row[0])
+    total_months = total_months + 1
+    profits.append(row[1])
+    net_total = net_total + (int(row[1]))
 
-    total_months += 1
-    total_pl += int(first_row[1])
-    value = int(first_row[1])
+    average_change = sum(profits)/len(profits)
+    
+    greatest_increase = max(profits)
+    greatest_index = profits.index(greatest_increase)
+    greatest_date = date[greatest_index]
+
+    greatest_decrease = min(profits)
+    decrease_index = profits.index(greatest_decrease)
+    decrease_date = date[worst_index]
+
+    print(f"Financial Analysis")
+    print(f"-------------------")
+
+    
+
+    
